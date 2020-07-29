f450cfce8b7bb4dfff645d56e904954915fedf62
Friday Saarelaht <19504141+darrowby385@users.noreply.github.com>
2020-07-29 15:54:00 +1000

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ if 5 > 2:
 def another_test():
 if 8 > 10:
   print("Eight is greater than ten?!")
+  
+def only_in_test_dot_py():
+if 30 > 20:
+  print("Thirty is greaty than twenty.")
 
 main()
 another_test()
+only_in_test()
+only_in_test_dot_py()
